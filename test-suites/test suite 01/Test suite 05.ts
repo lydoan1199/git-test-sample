139e1facbd384192733504c2497c342a3e87d99f
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-06 17:03:19 +0700

    commit test suite

---
+++ test-suites/test suite 01/Test suite 05.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Test suite 05</name>
+    <testSuiteGuid>0c0cef8a-d009-4881-a4fd-faa51f43547d</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Test Ly</testCaseId>
+        <guid>fc49ddf6-670f-4d45-b96c-bf56a4ac4155</guid>
+    </testCaseLink>
+</TestSuiteEntity>
