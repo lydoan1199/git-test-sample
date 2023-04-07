d215b6d34fbd39de2e7ba10154c698a7638e6da2
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-07 11:08:28 +0700

    Katalon: Update test suite Test suite 02 in test-suites/test suite 01/Test

--- test-suites/test suite 01/Test/Test suite 02.ts
+++ test-suites/test suite 01/Test/Test suite 02.ts
@@ -1,13 +1,41 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>Test suite 02</name>
-    <testSuiteGuid>68ed7316-9f6a-466a-b0ba-8c7ac5d5186b</testSuiteGuid>
+    <testSuiteGuid>2c8d3e75-6295-4f13-a00c-806ee079e475</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/New test case (3)</testCaseId>
-        <guid>066a613f-29b9-4094-8c34-273a08477533</guid>
+        <guid>62977e2c-d3d0-4058-a2bb-da170fa8f821</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/New test case (3)</testCaseId>
-        <guid>a0956202-0640-446c-ba6a-54cd51696319</guid>
+        <guid>5475160c-7ed8-4549-bbd8-ad342d1dbe3b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (2)</testCaseId>
+        <guid>4008f105-be83-4e81-be08-c0ded1561053</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (2)</testCaseId>
+        <guid>e39dc7ec-1c99-4543-89ec-ecaabdb20c67</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/Untitled folder/Untitled folder/Untitled folder/Untitled folder/hihi</testCaseId>
+        <guid>6cc250b6-7298-44b5-a908-f3baf424e33d</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Test CaseABC</testCaseId>
+        <guid>41c34f4b-209f-4b99-b4ef-f881fefe5913</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Test CaseABC</testCaseId>
+        <guid>f705067c-8119-4013-97ad-32ab77503d61</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/tuan/Test CaseTuanTest</testCaseId>
+        <guid>48b557f0-56fc-4da0-a141-f57385d55d66</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Test Ly</testCaseId>
+        <guid>4d7f1046-a67c-42e1-b856-08c87310a153</guid>
     </testCaseLink>
 </TestSuiteEntity>
