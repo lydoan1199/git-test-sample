0f5ccab4a0212e2765f59504087bdcbb59c1845d
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-07 11:06:05 +0700

    tessr

---
+++ test-suites/test suite 01/Test/Test suite 02.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Test suite 02</name>
+    <testSuiteGuid>68ed7316-9f6a-466a-b0ba-8c7ac5d5186b</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (3)</testCaseId>
+        <guid>066a613f-29b9-4094-8c34-273a08477533</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case (3)</testCaseId>
+        <guid>a0956202-0640-446c-ba6a-54cd51696319</guid>
+    </testCaseLink>
+</TestSuiteEntity>
