647c4ff1fed7b58ff35604fecc060cf2a214e4fd
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-28 17:06:28 +0700

    test

---
+++ test-cases/Untitled folder/New test case (5).ks.ts
@@ -0,0 +1,38 @@
+import katalon from "katalon";
+
+export default katalon.testCase("New Test Case", async ({ local, web }) => {
+  await new Promise((r) => setTimeout(r, 0));
+  await web.openBrowser("https://katalon-demo-cura.herokuapp.com");
+  await web.click({
+    type: "Web",
+    id: "857223b8-4462-47a3-b145-5a1e3cd8e055",
+    attributes: {
+      id: "btn-make-appointment",
+      href: "./profile.php#login",
+      class: "btn btn-dark btn-lg",
+    },
+    childIndex: 4,
+    hashes: { "md5.v1": "3caec008087ccb7a1cb3fc698fbf83c2" },
+    name: "a - Make Appointment",
+    selectors: [
+      {
+        id: "6fe2b646-4cda-4a04-9c6d-fb00eb859eb1",
+        type: "CSS",
+        value: "#btn-make-appointment",
+        isDefault: true,
+      },
+      {
+        id: "d48d31ae-e140-459b-8a13-78f510cbc183",
+        type: "Attribute",
+        value: null,
+        isDefault: true,
+      },
+    ],
+    selectorType: "CSS",
+    tag: "a",
+    text: "Make Appointment",
+    pageUrl: "https://katalon-demo-cura.herokuapp.com/",
+    parentIframe: null,
+    shadowRoot: null,
+  });
+});
