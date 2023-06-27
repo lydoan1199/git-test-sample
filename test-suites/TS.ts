8e506ba16b0d4bf02fe70c9063112d238a3cb9da
lydoan1199 <ly.doan@katalon.com>
2023-06-27 17:58:15 +0700

    Huhu

---
+++ test-suites/TS.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>TS</name>
+    <testSuiteGuid>e6f73a6f-1dd4-4a1f-b394-564d6b228c31</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/Untitled folder/Untitled folder/Untitled folder/Untitled folder/hihi</testCaseId>
+        <guid>d5ed64bc-21b3-439d-97e4-0eea748e5690</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/New test case</testCaseId>
+        <guid>b2155782-8a09-43cc-9065-dace3ea45e30</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/New test case (5)</testCaseId>
+        <guid>eaf2ebe0-904d-4924-a15f-291835d42e96</guid>
+    </testCaseLink>
+</TestSuiteEntity>
