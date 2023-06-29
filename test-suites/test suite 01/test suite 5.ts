45fb6eca66066b5b827a1b94e20aa1737a64761a
lydoan1199 <ly.doan@katalon.com>
2023-06-29 15:21:53 +0700

    commit

--- test-suites/test suite 01/test suite 5.ts
+++ test-suites/test suite 01/test suite 5.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>test suite 5</name>
-    <testSuiteGuid>eba81f8f-ee0f-4489-ab46-189c25dd85ee</testSuiteGuid>
+    <testSuiteGuid>63bea0d7-a749-49d8-b659-0b665a8d93ba</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/Untitled folder/Untitled folder/Untitled folder/Untitled folder/Untitled folder/hihi</testCaseId>
-        <guid>cb7c2a86-c94d-47e6-8197-7ee4bf062d3f</guid>
+        <guid>e8ae7a55-a464-4fd2-85e9-5fc97f878c67</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Untitled folder/New test case</testCaseId>
-        <guid>6cacb3ec-3788-41e5-b993-08a27279381f</guid>
+        <guid>5b58cccb-9929-413f-b48b-6b4f310cfe1d</guid>
     </testCaseLink>
     <testCaseLink>
         <testCaseId>test-cases/Untitled folder/New test case (5)</testCaseId>
-        <guid>4fa9a63e-099d-49d0-a18d-744f38aeeb11</guid>
+        <guid>fe95fa56-44b7-40d9-998d-ff94c6d731e5</guid>
     </testCaseLink>
 </TestSuiteEntity>
