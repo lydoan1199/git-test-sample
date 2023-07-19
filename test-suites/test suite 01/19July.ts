2075c4069743da65a6a4d6256c2701746e4dbb1c
lydoan1199 <ly.doan@katalon.com>
2023-07-19 11:53:57 +0700

    commit

---
+++ test-suites/test suite 01/19July.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>19July</name>
+    <testSuiteGuid>82946fb0-8592-4302-a7b1-528b335283a0</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/Untitled folder/Untitled folder/Untitled folder/Untitled folder/hihi</testCaseId>
+        <guid>142ebffd-0f68-4911-90c9-716f96e59bdc</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/New test case</testCaseId>
+        <guid>6953e812-d7be-4da8-a208-d6430bc7d55b</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Untitled folder/New test case (5)</testCaseId>
+        <guid>1d006949-316f-4894-a9c0-6a2eccb3eaec</guid>
+    </testCaseLink>
+</TestSuiteEntity>
